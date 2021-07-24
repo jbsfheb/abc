a25a57113738c90f9f8e60b04ce9d4edbd9a619c
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-07-24 15:07:07 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=SHIB:0xd32ff86c759aa0b6ad7f2ed6dc2b34d64d5f87c9
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#8i9a-v6zq)
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#0o44-e0mu)
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
